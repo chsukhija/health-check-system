0aeae3ae37ad6cc789dd610c4a1602e06f3c3b32
Chirag Sukhija <95433659+chsukhija@users.noreply.github.com>
2025-03-09 12:22:02 +0530

    refactor

--- solutions/main.tf
+++ solutions/main.tf
@@ -1,158 +1,168 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-  }
-}
-
-# Data Sources
-data "aws_eks_cluster" "main" {
-  name = var.eks_cluster_name
-}
-
-data "aws_eks_cluster_auth" "main" {
-  name = var.eks_cluster_name
-}
-
+################################################################################
 # Providers
+################################################################################
+
+
 provider "aws" {
-  region = var.region
-  default_tags {
-    tags = local.tags
-  }
+ region = var.region
+ default_tags {
+   tags = local.aws_default_tags
+ }
 }
+
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
+ host                   = data.aws_eks_cluster.main.endpoint
+ cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+ exec {
+   api_version = "client.authentication.k8s.io/v1beta1"
+   command     = "aws"
+   args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
+ }
 }
+
 
 provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.main.token
-  }
+ kubernetes {
+   host                   = data.aws_eks_cluster.main.endpoint
+   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+   exec {
+     api_version = "client.authentication.k8s.io/v1alpha1"
+     command     = "aws"
+     args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
+   }
+ }
 }
 
+
+data "aws_eks_cluster" "main" {
+ name = var.eks_cluster_name
+}
+
+
+################################################################################
 # Local Variables
+################################################################################
+
+
 locals {
-  name   = var.eks_cluster_name
-  region = var.region
-
-  tags = merge(
-    var.tags,
-    {
-      Name        = local.name
-      Environment = var.environment
-      Terraform   = "true"
-    }
-  )
+ aws_default_tags = merge(
+   {"clusterName": var.eks_cluster_name},
+   var.aws_default_tags,
+ )
+ customer_identifier         = trimprefix(var.eks_cluster_name, "wv-")
+ customer_cluster_identifier = "prod-dedicated-enterprise"
 }
 
-# Weaviate Module
-module "weaviate" {
-  source = "./modules/weaviate"
 
-  namespace = var.namespace
-  
-  config = {
-    replica_count = var.weaviate.replica_count
-    resources     = var.weaviate.resources
-    storage       = var.weaviate.storage
-    monitoring    = var.weaviate.monitoring
-    env_vars     = var.weaviate.env_vars
-  }
+################################################################################
+# EKS Cluster and VPC Configuration
+################################################################################
 
-  depends_on = [
-    kubernetes_namespace.weaviate
-  ]
+
+# (Intentionally left blank for exercise)
+
+
+################################################################################
+# High Availability Setup for Weaviate
+################################################################################
+
+
+module "weaviate_helm" {
+ source                        = "./modules/weaviate"
+  # ... (existing configuration)
+
+
+ # HA Setup
+ replica_count                 = 2 
+
+
+ # Weaviate-specific configurations
+ weaviate_replication_factor   = 1 
+
+
+ # Node Affinity and Anti-Affinity (to spread pods across AZs)
+ affinity = {
+   podAntiAffinity = {
+     requiredDuringSchedulingIgnoredDuringExecution = [
+       {
+         labelSelector = {
+           matchExpressions = [
+             {
+               key      = "app"
+               operator = "In"
+               values   = ["weaviate"]
+             },
+           ]
+         },
+         topologyKey = "topology.kubernetes.io/zone"
+       },
+     ]
+   }
+ }
+
+
+ # Tolerations (if needed based on your taints setup)
+ tolerations = [
+   # Tolerations configuration here
+ ]
+
+
+ # Persistent Volume Claim Issue
+ volume_claim_templates = [{
+   metadata = {
+     name = "weaviate-data"
+   }
+   spec = {
+     accessModes = ["ReadWriteOnce"]
+     resources = {
+       requests = {
+         storage = "10Gi"
+       }
+     }
+   }
+ }]
+
+
+ depends_on = [
+   kubernetes_namespace.weaviate-namespace
+ ]
 }
 
-# Kubernetes Resources
-resource "kubernetes_namespace" "weaviate" {
-  metadata {
-    name = var.namespace
-    labels = {
-      name = var.namespace
-    }
-  }
+
+################################################################################
+# Additional Modules and Resources
+################################################################################
+
+
+# What additional modules should be added here?
+
+
+################################################################################
+# Kubernetes Namespace for Weaviate
+################################################################################
+
+
+resource "kubernetes_namespace" "weaviate-namespace" {
+ metadata {
+   name = var.namespace
+ }
 }
 
-resource "kubernetes_config_map" "weaviate_monitoring" {
-  metadata {
-    name      = "weaviate-monitoring"
-    namespace = kubernetes_namespace.weaviate.metadata[0].name
-    labels    = local.tags
-  }
 
-  data = {
-    "monitoring.yaml" = yamlencode({
-      monitoring = {
-        enabled = true
-        prometheus = {
-          enabled = true
-        }
-        thresholds = var.weaviate.monitoring
-      }
-    })
-  }
-}
 
-resource "kubernetes_horizontal_pod_autoscaler_v2" "weaviate" {
-  metadata {
-    name      = "weaviate"
-    namespace = kubernetes_namespace.weaviate.metadata[0].name
-    labels    = local.tags
-  }
 
-  spec {
-    scale_target_ref {
-      api_version = "apps/v1"
-      kind        = "StatefulSet"
-      name        = "weaviate"
-    }
 
-    min_replicas = var.weaviate.autoscaling.min_replicas
-    max_replicas = var.weaviate.autoscaling.max_replicas
 
-    metric {
-      type = "Resource"
-      resource {
-        name = "cpu"
-        target {
-          type                = "Utilization"
-          average_utilization = var.weaviate.monitoring.cpu_threshold
-        }
-      }
-    }
 
-    metric {
-      type = "Resource"
-      resource {
-        name = "memory"
-        target {
-          type                = "Utilization"
-          average_utilization = var.weaviate.monitoring.memory_threshold
-        }
-      }
-    }
 
-    behavior {
-      scale_up   = var.weaviate.autoscaling.scale_up
-      scale_down = var.weaviate.autoscaling.scale_down
-    }
-  }
-}
+
+
+
+
+
+
+
+
+
+
