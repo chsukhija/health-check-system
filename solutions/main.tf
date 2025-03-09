f0ff7cf24f1ef806dd18adc026b85917c5f0363e
Chirag Sukhija <95433659+chsukhija@users.noreply.github.com>
2025-03-09 12:36:36 +0530

    refactor

--- solutions/main.tf
+++ solutions/main.tf
@@ -59,8 +59,167 @@ locals {
 # EKS Cluster and VPC Configuration
 ################################################################################
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
 
-# (Intentionally left blank for exercise)
+  name = "${var.eks_cluster_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets = var.private_subnet_cidrs
+  public_subnets  = var.public_subnet_cidrs
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false  # HA setup with NAT gateway per AZ
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
+
+  # Tags required for EKS
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name                   = var.eks_cluster_name
+  cluster_version               = var.kubernetes_version
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # Enable encryption for secrets
+  cluster_encryption_config = [{
+    provider_key_arn = aws_kms_key.eks.arn
+    resources        = ["secrets"]
+  }]
+
+  # EKS Managed Node Group
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 3
+      min_size     = 3
+      max_size     = 5
+
+      instance_types = ["t3.xlarge"]
+      capacity_type  = "ON_DEMAND"
+
+      # Enable node-to-node encryption
+      enable_bootstrap_user_data = true
+      bootstrap_extra_args      = "--enable-docker-bridge true --kubelet-extra-args '--node-labels=node.kubernetes.io/lifecycle=normal'"
+
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size           = 100
+            volume_type          = "gp3"
+            iops                 = 3000
+            encrypted           = true
+            delete_on_termination = true
+          }
+        }
+      }
+
+      tags = {
+        "k8s.io/cluster-autoscaler/enabled" = "true"
+        "k8s.io/cluster-autoscaler/${var.eks_cluster_name}" = "owned"
+      }
+    }
+  }
+
+  # IRSA for cluster autoscaler
+  enable_irsa = true
+
+  # Node security group additional rules
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+  }
+}
+
+# KMS key for EKS cluster encryption
+resource "aws_kms_key" "eks" {
+  description             = "EKS Secret Encryption Key"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = {
+    Name = "${var.eks_cluster_name}-eks-encryption-key"
+  }
+}
+
+# Cluster Autoscaler IAM Role
+module "cluster_autoscaler_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name                        = "cluster-autoscaler"
+  attach_cluster_autoscaler_policy = true
+  cluster_autoscaler_cluster_ids   = [module.eks.cluster_id]
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:cluster-autoscaler"]
+    }
+  }
+}
+
+# CloudWatch Log Group for EKS cluster logs
+resource "aws_cloudwatch_log_group" "eks" {
+  name              = "/aws/eks/${var.eks_cluster_name}/cluster"
+  retention_in_days = 30
+}
+
+# Additional security group rules for control plane
+resource "aws_security_group_rule" "eks_api_private" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = module.eks.cluster_security_group_id
+  source_security_group_id = module.eks.node_security_group_id
+  description             = "Allow nodes to communicate with the cluster API Server"
+}
+
+# Outputs
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_iam_role_name" {
+  description = "IAM role name associated with EKS cluster"
+  value       = module.eks.cluster_iam_role_name
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
 
 
 ################################################################################
--- solutions/variables.tf
+++ solutions/variables.tf
@@ -1,13 +1,6 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
-}
-
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "prod"
 }
 
 variable "eks_cluster_name" {
@@ -15,112 +8,26 @@ variable "eks_cluster_name" {
   type        = string
 }
 
-variable "namespace" {
-  description = "Kubernetes namespace for Weaviate"
+variable "kubernetes_version" {
+  description = "Kubernetes version"
   type        = string
-  default     = "weaviate"
+  default     = "1.27"
 }
 
-variable "tags" {
-  description = "Additional tags for resources"
-  type        = map(string)
-  default     = {}
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "weaviate" {
-  description = "Weaviate configuration"
-  type = object({
-    replica_count = number
-    resources = object({
-      limits = object({
-        cpu    = string
-        memory = string
-      })
-      requests = object({
-        cpu    = string
-        memory = string
-      })
-    })
-    storage = object({
-      size          = string
-      storage_class = string
-    })
-    monitoring = object({
-      memory_threshold = number
-      cpu_threshold    = number
-    })
-    env_vars = map(string)
-    autoscaling = object({
-      min_replicas = number
-      max_replicas = number
-      scale_up = object({
-        stabilization_window_seconds = number
-        select_policy               = string
-        policies = list(object({
-          type           = string
-          value         = number
-          period_seconds = number
-        }))
-      })
-      scale_down = object({
-        stabilization_window_seconds = number
-        select_policy               = string
-        policies = list(object({
-          type           = string
-          value         = number
-          period_seconds = number
-        }))
-      })
-    })
-  })
-  default = {
-    replica_count = 2
-    resources = {
-      limits = {
-        cpu    = "4000m"
-        memory = "16Gi"
-      }
-      requests = {
-        cpu    = "2000m"
-        memory = "8Gi"
-      }
-    }
-    storage = {
-      size          = "100Gi"
-      storage_class = "gp3"
-    }
-    monitoring = {
-      memory_threshold = 80
-      cpu_threshold    = 75
-    }
-    env_vars = {
-      ENABLE_MODULES                         = "backup-filesystem,text2vec-transformers"
-      QUERY_DEFAULTS_LIMIT                   = "25"
-      AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED = "false"
-      PERSISTENCE_DATA_PATH                  = "/var/lib/weaviate"
-      LIMIT_RESOURCES                        = "true"
-    }
-    autoscaling = {
-      min_replicas = 2
-      max_replicas = 5
-      scale_up = {
-        stabilization_window_seconds = 300
-        select_policy               = "Max"
-        policies = [{
-          type           = "Pods"
-          value         = 2
-          period_seconds = 300
-        }]
-      }
-      scale_down = {
-        stabilization_window_seconds = 300
-        select_policy               = "Min"
-        policies = [{
-          type           = "Pods"
-          value         = 1
-          period_seconds = 300
-        }]
-      }
-    }
-  }
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
